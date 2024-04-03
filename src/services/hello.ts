13413ef0fd72f90f5b0d228701ede0eae1c0436e
bronifty <bronifty@gmail.com>
2024-04-03 14:24:00 -0400

    this

---
+++ src/services/hello.ts
@@ -0,0 +1,46 @@
+// import {
+//   APIGatewayProxyEvent,
+//   APIGatewayProxyResult,
+//   Context,
+// } from "aws-lambda";
+// import * as uuid from "uuid";
+
+// async function handler(event: APIGatewayProxyEvent, context: Context) {
+//   const response: APIGatewayProxyResult = {
+//     statusCode: 200,
+//     body: JSON.stringify(`Hello from Lambda! ${process.env.TABLE_NAME}`),
+//   };
+//   console.log(event);
+
+//   return response;
+// }
+
+// export { handler };
+
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Context,
+} from "aws-lambda";
+import { v4 } from "uuid";
+import { S3Client, ListBucketsCommand } from "@aws-sdk/client-s3";
+
+const s3Client = new S3Client({});
+
+async function handler(event: APIGatewayProxyEvent, context: Context) {
+  const command = new ListBucketsCommand({});
+  const listBucketsResult = (await s3Client.send(command)).Buckets;
+
+  const response: APIGatewayProxyResult = {
+    statusCode: 200,
+    body: JSON.stringify(
+      "Hello from lambda, here are your buckets:" +
+        JSON.stringify(listBucketsResult)
+    ),
+  };
+  console.log(event);
+
+  return response;
+}
+
+export { handler };

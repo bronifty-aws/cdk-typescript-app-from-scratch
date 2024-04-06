fcd0e42502cae972403256e524c0f07d98e5d1a7
bronifty <bronifty@gmail.com>
2024-04-05 21:01:29 -0400

    this

--- src/infra/stacks/LambdaStack.ts
+++ src/infra/stacks/LambdaStack.ts
@@ -18,7 +18,7 @@ export class LambdaStack extends cdk.Stack {
       {
         runtime: cdk.aws_lambda.Runtime.NODEJS_LATEST,
         handler: "handler",
-        entry: path.join(__dirname, "../../services/index.ts"),
+        entry: path.join(__dirname, "../../services/spaces/handler.ts"),
         environment: {
           TABLE_NAME: props.spacesTable.tableName,
         },
